0e1ddc12f31738fed473912eb2c2305745f2d0b0
Fabian <ianyudiardi@gmail.com>
2025-05-08 23:30:07 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// filepath: d:\!AMIKOM\SEMESTER 6\Digital Business\Cardinary\src\App.jsx
+import React from 'react';
+
+//import pages
+import Landing from './pages/Landing';
+
+export default function App(){
+  return (
+    <div>
+      <Landing />
+    </div>
+  );
+};
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Landing() {
+    return (
+        <div className="container flex justify-center align-center h-screen w-screen bg-gray-200">
+            <div className="flex justify-center align-center h-48 w-48 bg-red-500"></div>
+        </div>
+    );
+}
